dab51a484a4e9a67d8ae2b94243ecaedcdd64182
JarousNemec <jarous.nemec@gmail.com>
2026-09-22 15:26:01 +0200

    complete guess the number game

--- GuessTheNumber/GuessTheNumber.hs
+++ GuessTheNumber/GuessTheNumber.hs
@@ -3,14 +3,32 @@ module GuessTheNumber where
 import System.Random (randomRIO)
 {-
     The lib random isnt in the GHC environment by default so you need to install is manualy
-    
-    cabal update
-    cabal install --lib random
+    Enter following lines to your bash:
+
+    > cabal update
+    > cabal install --lib random
 -}
+
+stringToInt :: String -> Int
+stringToInt s = read s
+
+guess :: Int -> IO()
+guess n = do
+    input <- getLine
+    let inputNumber = stringToInt input
+    if inputNumber == n 
+        then do
+            putStrLn "Correct."
+        else do
+            putStrLn "Incorrect!" 
+            putStrLn "Enter new number:" 
+            guess n
 
 main :: IO ()
 main = do
     randomNumber <- randomRIO(1, 10) :: IO Int
-    print randomNumber
-    putStrLn "Guess the number from 1 to 10"
-    
+    putStrLn "Guess the number from 1 to 10:"
+    guess randomNumber
+    putStrLn "Congratulation!!"
+
+
